7914623bc290256a030dae88bfbbfb1f264853de
Manuelkpatsu <manuelkpatsu@gmail.com>
2021-11-20 14:58:28 +0000

    Add screens

---
+++ lib/view/screens/recipe_list_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RecipeListScreen extends StatelessWidget {
+  const RecipeListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Recipe List Screen'),
+      ),
+    );
+  }
+}
---
+++ lib/view/screens/recipe_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RecipeScreen extends StatelessWidget {
+  const RecipeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Recipe Screen'),
+      ),
+    );
+  }
+}
