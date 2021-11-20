d2f5901bea614f1cb27fa8a6442452e2907765cc
Manuelkpatsu <manuelkpatsu@gmail.com>
2021-11-20 15:23:15 +0000

    Add widgets

--- lib/view/screens/recipe_screen.dart
+++ lib/view/screens/recipe_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutterslivers/models/recipe_model.dart';
 
 class RecipeScreen extends StatelessWidget {
-  const RecipeScreen({Key? key}) : super(key: key);
+  final RecipeModel recipe;
+
+  const RecipeScreen(this.recipe, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
